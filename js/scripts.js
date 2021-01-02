0e9cf56400181335a769d8280ecaa479ab760843
Marvin A <mka507@gmail.com>
2021-01-02 12:29:55 -0500

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,16 @@ $(function () {
         }
     });
 });
+
+
+$(function () {
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal("toggle");
+    });
+});
+
+$(function () {
+    $("#loginButton").click(function () {
+        $("#loginModal").modal("toggle");
+    });
+});
